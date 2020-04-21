021399fe2f99f4eb5c23dad04659cbfd121dfd53
olkovmv <olkovmav@gmail.com>
2020-04-22 01:36:14 +0300

    Hello

--- src/Lesson14/hw/Employee.java
+++ src/Lesson14/hw/Employee.java
@@ -6,6 +6,8 @@ package Lesson14.hw;
 // имени и зарплете
 // имени, зарплете, возрасту и компании
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +17,61 @@ public class Employee {
     private int salary;
     private int age;
 
-    // TODO: конструктор, геттеры и сеттеры
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", company='" + company + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+    public Employee(String name, String company, int salary, int age) {
+        this.name = name;
+        this.company = company;
+        this.salary = salary;
+        this.age = age;
+
+
+
+    }
+
 
     public static List<Employee> employeeGenerator(int num) {
         // метод для создания списка объектов класса Employee
@@ -24,10 +80,13 @@ public class Employee {
 
         List<Employee> employees = new ArrayList<>(num);
 
+
         // добавление num объектов Employee в список (employees)
         for (int i = 0; i < num; i++) {
-            employees.add(new Employee(   )); // TODO: объекты создавать с рандомными значениями. Возраст от 21 до 60 и не забудьте про зп
+            employees.add(new Employee(names[i],companies[i], (int) Math.random()*800+600,(int)Math.random()*39+21)); // TODO: объекты создавать с рандомными значениями. Возраст от 21 до 60 и не забудьте про зп
         }
         return employees;
+
     }
+
 }
---
+++ src/Lesson14/hw/MainEmpoyee.java
@@ -0,0 +1,18 @@
+package Lesson14.hw;
+
+import Lesson14.Student;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainEmpoyee {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+
+        employee.
+
+
+    }
+}
